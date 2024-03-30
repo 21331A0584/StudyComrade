7234715acdbc8534ff4248fa2112cdd5e5821f2f
21331A0584 <kodidasusaimanikanta8341@gmail.com>
2024-03-30 18:26:57 +0530

    Added Final Changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,20 @@
+const form = document.getElementById('myForm');
+
+// Add event listener for form submission
+form.addEventListener('submit', function(event) {
+    // Get the year and semester select elements
+    const yearSelect = document.getElementById('year');
+    const semesterSelect = document.getElementById('semester');
+    
+    // Check if year or semester is not selected
+    if (yearSelect.value === '' || semesterSelect.value === '') {
+        // Prevent the default form submission
+        event.preventDefault();
+        
+        // Display a message to the user
+        alert('Please select both year and semester.');
+    }
+});
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -34,3 +51,51 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+
+
+// Navbar
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+// Dark Mode
+let darkmode = document.querySelector('#darkmode');
+
+
+darkmode.onclick = () => {
+    if(darkmode.classList.contains('bx-moon')){
+        darkmode.classList.replace('bx-moon','bx-sun');
+        document.body.classList.add('active');
+    }else{
+        darkmode.classList.replace('bx-sun','bx-moon');
+        document.body.classList.remove('active');
+    }
+}
+
+
+// Scroll Reveal
+const sr = ScrollReveal ({
+    origin: 'top',
+    distance: '40px',
+    duration: 2000,
+    reset: true
+});
+
+
+
+
+sr.reveal(`.home-text, .home-img,
+            .about-img, .about-text,
+            .box, .s-box,
+            .btn, .connect-text,
+            .contact-box`, {
+    interval: 200
+})
